17cb786dc08f3b451ce3127f9a4d801c628a50fb
Larry <Larry>
2017-01-05 17:24:34 +0800

    First

---
+++ app/src/main/java/com/larry/demoluban/MainActivity.java
@@ -0,0 +1,130 @@
+package com.larry.demoluban;
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.larry.luban.Luban;
+import com.larry.luban.OnCompressListener;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int OPEN_GALLERY_CODE = 0x001;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(this);
+    }
+
+    /**
+     * 打开相册
+     */
+    public void openGallery() {
+//        tempFile="a.jpeg";
+        Intent intent = new Intent(Intent.ACTION_PICK);// 打开相册
+        intent.setDataAndType(MediaStore.Images.Media.INTERNAL_CONTENT_URI, "image/*");
+//        intent.putExtra("output", Uri.fromFile(tempFile));
+        startActivityForResult(intent, OPEN_GALLERY_CODE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        openGallery();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("way","origin size ="+getFileByUri(data.getData()).getUsableSpace());
+        Luban.get(this).load(getFileByUri(data.getData())).putGear(Luban.THIRD_GEAR)
+                .setCompressListener(new OnCompressListener() {
+                    @Override
+                    public void onStart() {
+                    }
+
+                    @Override
+                    public void onSuccess(final File file) {
+                        Log.i("way","origin size ="+file.getUsableSpace());
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Picasso.with(MainActivity.this).load(file).into(imageView);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.i("way", "error");
+                    }
+                }).launch();
+    }
+
+    /**
+     * 通过Uri返回File文件
+     * 注意：通过相机的是类似content://media/external/images/media/97596
+     * 通过相册选择的：file:///storage/sdcard0/DCIM/Camera/IMG_20150423_161955.jpg
+     * 通过查询获取实际的地址
+     *
+     * @param uri
+     * @return
+     */
+    public File getFileByUri(Uri uri) {
+        String path = null;
+        if ("file".equals(uri.getScheme())) {
+            path = uri.getEncodedPath();
+            if (path != null) {
+                path = Uri.decode(path);
+                ContentResolver cr = getContentResolver();
+                StringBuffer buff = new StringBuffer();
+                buff.append("(").append(MediaStore.Images.ImageColumns.DATA).append("=").append("'" + path + "'").append(")");
+                Cursor cur = cr.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Images.ImageColumns._ID, MediaStore.Images.ImageColumns.DATA}, buff.toString(), null, null);
+                int index = 0;
+                int dataIdx = 0;
+                for (cur.moveToFirst(); !cur.isAfterLast(); cur.moveToNext()) {
+                    index = cur.getColumnIndex(MediaStore.Images.ImageColumns._ID);
+                    index = cur.getInt(index);
+                    dataIdx = cur.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    path = cur.getString(dataIdx);
+                }
+                cur.close();
+                if (index == 0) {
+                } else {
+                    Uri u = Uri.parse("content://media/external/images/media/" + index);
+                    System.out.println("temp uri is :" + u);
+                }
+            }
+            if (path != null) {
+                return new File(path);
+            }
+        } else if ("content".equals(uri.getScheme())) {
+            // 4.2.2以后
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(uri, proj, null, null, null);
+            if (cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                path = cursor.getString(columnIndex);
+            }
+            cursor.close();
+
+            return new File(path);
+        } else {
+            Log.i("way", "Uri Scheme:" + uri.getScheme());
+        }
+        return null;
+    }
+}
